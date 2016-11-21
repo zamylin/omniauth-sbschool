f2d98d3982e16d36f8255fa78e9bd3bd97356eb6
t.kosminov <t.kosminov@oblakogroup.ru>
2016-11-21 17:10:35 +0300

    first commit

--- lib/omniauth/strategies/sbschool.rb
+++ lib/omniauth/strategies/sbschool.rb
@@ -3,6 +3,27 @@ require 'omniauth-oauth2'
 module Omniauth
 	module Strategies
 		module Sbschool < OmniAuth::Strategies::OAuth2
+      option :name, 'sbschool'
+
+      option :client_options, {
+        :site => 'https://api.sberbank-school.ru/'
+        :authorization_url => 'https://api.sberbank-school.ru/oauth/authorize/'
+        :token_url => 'https://api.sberbank-school.ru/oauth/access_token/'
+      }
+
+      uid { raw_info['client_id'] }
+
+      extra do
+        { raw_info: raw_info }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/').parsed || {}
+      end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
 
 		end
 	end
